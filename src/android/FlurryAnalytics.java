a150a672a6ba250d886a1f0b887f96c3020cd2db
Mikhail Kiselev <mikhail.kiselev@simbirit.ru>
2019-01-30 18:19:17 +0400

    in progress

--- src/android/FlurryAnalytics.java
+++ src/android/FlurryAnalytics.java
@@ -1,5 +1,8 @@
 package com.codemech.flurryanalytics;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -12,18 +15,53 @@ import org.json.JSONObject;
 
 import com.flurry.android.FlurryAgent;
 
-public class FlurryAnalytics extends CordovaPlugin 
+import android.app.Activity;
+
+public class FlurryAnalytics extends CordovaPlugin {
 
 	private static final String FLURRY_ID = "Your flurry identifier must be here"; 
+	private static final String LOG_EVENT = "logEvent";
+	private static final String END_TIMED_EVENT = "endTimedEvent";
 
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
-		cordova.getActivity().runOnUiThread(new Runnable(){
+		final Activity mainactivity = cordova.getActivity();
+		mainactivity.runOnUiThread(new Runnable(){
             @Override
             public void run() {
-				FlurryAgent.init(this, FLURRY_ID);
-				FlurryAgent.onStartSession(this, FLURRY_ID);
+            	
+				FlurryAgent.init(mainactivity, FLURRY_ID);
+				FlurryAgent.onStartSession(mainactivity, FLURRY_ID);
 			}
+			
+			
+			
 		});
+	}
+	public void onDestroy() {
+	    FlurryAgent.onEndSession(cordova.getActivity());
+	    super.onDestroy();
+    } 
+	@Override
+    public boolean execute(String action, JSONArray inputs, CallbackContext callbackContext) throws JSONException {
+        PluginResult result = null;
+        JSONObject options = inputs.optJSONObject(0);
+        if(LOG_EVENT.equals(action)){
+        	
+        	Map<String, String> articleParams = new HashMap<String, String>();
+        	if(options.has("type")){
+        		articleParams.put("type", options.optString("type"));
+        	}
+        	if(options.has("level")){
+        		articleParams.put("level", options.optString("level"));
+        	}
+        	
+        	FlurryAgent.logEvent("Article_Read", articleParams); 
+        	
+        } else if(END_TIMED_EVENT.equals(action)){
+        	
+        }
+        
+        return true;
 	}
 }
